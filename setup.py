fc353ae77c75568a8cdbecdbb25ab61fad24ca26
Arek Korbik <arkadini@gmail.com>
2011-03-15 23:02:21 +0100

    Distutil-ize the library.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+"""twimp
+=====
+
+Another python RTMP implementation: this one is supposed to match
+the twisted design principles closer than other implementations.
+"""
+
+from distutils.core import setup
+
+setup(name='twimp',
+      version='0.1',
+      description='Twisted RTMP implementation',
+      long_description=__doc__,
+      platforms=['any'],
+      license='Apache License v2.0',
+      author='Arek Korbik',
+      author_email='arkadini@gmail.com',
+      maintainer='Arek Korbik',
+      maintainer_email='arkadini@gmail.com',
+      url='http://github.com/arkadini/twimp',
+      packages=['rtmp', 'rtmp.crypto', 'rtmp.server', 'rtmp.scripts'])
